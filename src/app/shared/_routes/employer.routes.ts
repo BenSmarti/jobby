a547ed183781609938877e1698a6683ef81608c9
daniellotem <daniellotem@smarti.co.il>
2018-02-04 10:39:22 +0200

    login page

--- src/app/shared/_routes/employer.routes.ts
+++ src/app/shared/_routes/employer.routes.ts
@@ -1,17 +1,12 @@
 import {Routes} from '@angular/router';
 import {LoginComponent} from '../../public/login/login.component';
 import {RegisterComponent} from '../../public/register/register.component';
+import {JobComponent} from '../../employer/job/job.component';
+import {CandidatesComponent} from '../../employer/candidates/candidates.component';
 
-<<<<<<< HEAD
 export const EMPLOYER_ROUTES: Routes = [
   {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent}
-=======
-import { JobComponent } from '../../employer/job/job.component';
-import { CandidatesComponent } from '../../employer/candidates/candidates.component';
-
-export const EMPLOYER_ROUTES: Routes = [
-  { path: 'jobs', component: JobComponent },
-  { path: 'candidates', component: CandidatesComponent },
->>>>>>> 62dd37b01d70a0623152afb25700c0ce9199fbb1
+  {path: 'register', component: RegisterComponent},
+  {path: 'jobs', component: JobComponent},
+  {path: 'candidates', component: CandidatesComponent},
 ];
