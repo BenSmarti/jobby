8eccf5ad0d62908c23a4a7092a3433f091d817a9
Ben Davidovich <bend@smarti.co.il>
2018-02-06 10:27:14 +0200

    added footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { TranslationsComponent } from '../shared/translations/translations.component';
+
+import { LocaleService } from '../shared/_services/locale.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class FooterComponent extends TranslationsComponent {
+  constructor(localeService: LocaleService) {
+    super(localeService);
   }
-
 }
