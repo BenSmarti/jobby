8e08a6e5b1ac08d7fd88bfc18d4c6e3ecfcf36b4
Ben Davidovich <bend@smarti.co.il>
2018-02-20 21:33:49 +0200

    fix in chat resolve

--- src/app/shared/_resolves/chat.resolve.ts
+++ src/app/shared/_resolves/chat.resolve.ts
@@ -6,11 +6,11 @@ import { ChatService } from '../_services/http/chat.service';
 import { Chat } from '../_models/chat.model';
 
 @Injectable()
-export class ChatResolve implements Resolve<Chat[]> {
+export class ChatResolve implements Resolve<Chat> {
 
   constructor(private chatService: ChatService) {}
 
   resolve(route: ActivatedRouteSnapshot) {
-    return this.chatService.getChat(+route.params['id']).then(response => response as Chat[]);
+    return this.chatService.getChat(+route.params['id']).then(response => response as Chat);
   }
 }
